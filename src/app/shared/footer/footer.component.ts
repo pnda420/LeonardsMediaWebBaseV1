6a2ec809e90f64f6834dc51a27e2c37d1cbb7717
Tom Leonards <tom.leonards2003@gmail.com>
2025-08-09 15:37:05 +0200

    213

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+   constructor(public router: Router) {}
  year = new Date().getFullYear();
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +9,6 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+   constructor(public router: Router) {}
   open = false;
 }
